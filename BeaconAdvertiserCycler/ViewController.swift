16acf89c276c1dc23d6ef17da2683020ad8905ef
David G. Young <dyoung@Davids-MBP.hitronhub.home>
2022-04-28 19:27:44 -0400

    Advertise with alternating minor but only five minutes at a time three times per hour.

--- BeaconAdvertiserCycler/ViewController.swift
+++ BeaconAdvertiserCycler/ViewController.swift
@@ -10,7 +10,7 @@ import CoreBluetooth
 import CoreLocation
 
 class ViewController: UIViewController, CBPeripheralManagerDelegate {
-    var minor: UInt16 = 1
+    var minor: UInt16? = 1 // nil means do not advertise at all
     var peripheralManager: CBPeripheralManager!
     var transmitterOn = false
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
@@ -24,31 +24,55 @@ class ViewController: UIViewController, CBPeripheralManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         peripheralManager =  CBPeripheralManager(delegate: self, queue: nil)
-        rotateAdvertisement()
+        rotateAdvertisementWhenTime()
     }
-    func rotateAdvertisement() {
+    var lastRotationNumberWithinHour = -1
+    func rotateAdvertisementWhenTime() {
         // Change the line below to adjust the number of seconds between rotations
-        let rotationPeriodSeconds = 60.0
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+rotationPeriodSeconds) {
-            self.stopTransmitter()            
-            // Uncomment this line to increment minor every
-            self.minor += 1
-            // Uncomment this line to alternate minior between 1 and 2
-            // self.minor = self.minor == 1 ? 2 : 1
-            self.startTransmitter()
-            self.rotateAdvertisement()
+        let rotationPeriodSeconds = 60
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+1.0) {
+            let secsSince1970 = Int(Date().timeIntervalSince1970)
+            let minuteWithinHour = (secsSince1970 % 3600) / 60
+            let rotationNumberWithinHour = (secsSince1970 % 3600) / rotationPeriodSeconds
+            // check to see if we have crossed the minute boundary, otherwise do nothing here.
+            if (rotationNumberWithinHour != self.lastRotationNumberWithinHour) {
+                self.lastRotationNumberWithinHour = rotationNumberWithinHour
+                self.stopTransmitter()
+                // Uncomment this block to increment minor every minute
+                //self.minor += 1
+                // Uncomment this block to alternate minior between 1 and 2
+                //self.minor = self.minor == 1 ? 2 : 1
+                // Uncomment this block to alternate minor between 1 and 2 only on HH:00-HH:04, HH20-HH:24, HH:40-HH:44 (for five minutes three times an hour)
+                if minuteWithinHour <= 4 || (minuteWithinHour >= 20 && minuteWithinHour <= 24) || (minuteWithinHour >= 40 && minuteWithinHour <= 44) {
+                    self.minor = UInt16(minuteWithinHour) % 2 + 1 // minor 1 on even minutes minor 2 on odd minutes
+                }
+                else {
+                    self.minor = nil
+                }
+                
+                if self.minor != nil {
+                    self.startTransmitter()
+                }
+            }
+            self.rotateAdvertisementWhenTime()
         }
     }
     
     func startTransmitter() {
         transmitterOn = true
         NSLog("Starting advertisting")
-        advertisingLabel.text = "ADVERTISING MINOR \(minor)"
-        advertisingLabel.textColor = .green
+        if let minor = minor {
+            advertisingLabel.text = "ADVERTISING MINOR \(minor)"
+            let uuid = UUID(uuidString: "00e69745-943a-45de-bf29-f80586c46c99")!
+            let data = CLBeaconRegion(proximityUUID: uuid, major: 1, minor: minor, identifier: "MyIdentifier").peripheralData(withMeasuredPower: nil)
+            peripheralManager.startAdvertising(data as? [String:Any])
+            advertisingLabel.textColor = .green
+        }
+        else {
+            advertisingLabel.text = "NOT ADVERTISING NOW"
+            advertisingLabel.textColor = .red
+        }
 
-        let uuid = UUID(uuidString: "00e69745-943a-45de-bf29-f80586c46c99")!
-        let data = CLBeaconRegion(proximityUUID: uuid, major: 1, minor: minor, identifier: "MyIdentifier").peripheralData(withMeasuredPower: nil)
-        peripheralManager.startAdvertising(data as? [String:Any])
     }
     func stopTransmitter() {
         transmitterOn = false
